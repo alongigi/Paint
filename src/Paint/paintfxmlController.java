a5808b0f14ff3eec0af0b6f76a9555cab35ada0e
alongigi <giga@post.bgu.ac.il>
2018-04-21 13:32:46 +0300

    paint

--- src/Paint/paintfxmlController.java
+++ src/Paint/paintfxmlController.java
@@ -1,12 +1,19 @@
 package Paint;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,12 +33,16 @@ public class paintfxmlController implements Initializable {
 
     GraphicsContext brushToll;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         brushToll = canvas.getGraphicsContext2D();
         canvas.setOnMouseDragged( e -> {
-            double size =Double.parseDouble(bsize.getText());
+            double size;
+            try{
+                size = Double.parseDouble(bsize.getText());
+            }catch (NumberFormatException ec){
+                size = 0;
+            }
             double x = e.getX() - size / 2;
             double y = e.getY() - size / 2;
 
@@ -41,6 +52,7 @@ public class paintfxmlController implements Initializable {
             }
         });
     }
+
     @FXML
     public void toolselected(ActionEvent e){
         toolSelected = true;
